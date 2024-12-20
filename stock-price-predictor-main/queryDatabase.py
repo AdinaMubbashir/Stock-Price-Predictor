c9d7402df8f1390a07c01bbb2a7a0086c06faabb
AdinaMubbashir <121837692+AdinaMubbashir@users.noreply.github.com>
2024-12-19 23:38:05 -0500

    Add files via upload

---
+++ stock-price-predictor-main/queryDatabase.py
@@ -0,0 +1,21 @@
+"""
+Purpose: This file views entries from the stocks_dev database.
+"""
+
+
+import sqlite3
+
+# Connects to the database and prints the first 3000 records in the terminal (for debugging and validation purposes)
+def viewEntries(db_path="stocks.db", limit=3000):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM stocks LIMIT {limit}")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    conn.close()
+
+viewEntries()
